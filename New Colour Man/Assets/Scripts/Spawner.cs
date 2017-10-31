a71a1152fdd079af9ee36a19c7fd78efefac776f
Kasper Emil <kasperemil38@gmail.com>
2017-10-31 15:12:22 +0100

    Spawner added

---
+++ New Colour Man/Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.Find("Ragdoll(Clone)") == null)
+        {
+            Instantiate(prefab, this.transform);
+           
+        }
+	}
+}
