82f51b65b8b8c1ff39d5b8eadede6b8061995295
Quaade94 <larsquaadeconnect@gmail.com>
2017-10-31 14:18:42 +0100

    Player collider færdig mangler kun maxspeed

--- New Colour Man/Assets/Scripts/RagdollController.cs
+++ New Colour Man/Assets/Scripts/RagdollController.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 
 public class RagdollController : MonoBehaviour {
 
-	public float upSpeed = 0.0015f;
 	public float speed = 10;
-	public float jumpSpeed = 8;
+	public float maxSpeed = 200;
+	public float upSpeed = 0.0015f;
 	public Text countText;
 	public Text doorText;
-	public Rigidbody MoveBone;
+	public Rigidbody RB;
+
 
 	private int count;	
 	private bool isGrounded;
+
 	// Use this for initialization
 	void Start () {
 
@@ -27,17 +29,18 @@ public class RagdollController : MonoBehaviour {
 	void Update () {
 		if(Input.GetKey ("d"))
 		{
-			MoveBone.AddForce (Vector3.right * 50*speed);
-			//transform.position = transform.position + Vector3.left*Speed ;
+			RB.AddForce (Vector3.right * 50*speed);
+			//transform.position = transform.position + Vector3.right*speed ;
 		}
 		if(Input.GetKey ("a"))
 		{
-			MoveBone.AddForce (Vector3.left * 50*speed);
-			//transform.position = transform.position + Vector3.right*Speed;
+			RB.AddForce (Vector3.left * 50*speed);
+			//transform.position = transform.position + Vector3.left*speed;
 		}
 		if (Input.GetKey ("space")){
 
-			MoveBone.AddForce (Vector3.up * 50*speed);
+			RB.AddForce (Vector3.up * 50*speed);
+
 			//GetComponent<Rigidbody>().velocity += jumpSpeed * Vector3.up;
 		}
 	}
@@ -45,6 +48,10 @@ public class RagdollController : MonoBehaviour {
 	{
 		GetComponent<Rigidbody>().AddForce (Vector3.up * upSpeed);
 
+		//if(RB.velocity.magnitude > maxSpeed)
+		//{
+			//RB.velocity = RB.velocity.normalized * maxSpeed;
+		//}
 
 	}
 	void OnTriggerEnter(Collider other) 
