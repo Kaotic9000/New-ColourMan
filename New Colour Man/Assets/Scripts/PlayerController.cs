24c436aab87d3bc1d97003d1ee2c622667e92171
Quaade94 <larsquaadeconnect@gmail.com>
2017-10-31 15:10:26 +0100

    Merge Conflict

--- New Colour Man/Assets/Scripts/PlayerController.cs
+++ New Colour Man/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float upSpeed = 0.0015f;
-<<<<<<< HEAD:New Colour Man/Assets/Scripts/PlayerController.cs
-
 	public Rigidbody RB;
-
-=======
+	public float maxSpeed = 200;
 	public float speed = 10;
-	public float jumpSpeed = 8;
-	public Text countText;
-	public Text doorText;
-	public Rigidbody MoveBone;
->>>>>>> origin/master:New Colour Man/Assets/Scripts/RagdollController.cs
 
 	private bool isGrounded;
 	// Use this for initialization
@@ -28,31 +19,25 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		if(Input.GetKey ("d"))
 		{
-			MoveBone.AddForce (Vector3.right * 50*speed);
+			RB.AddForce (Vector3.right * 50*speed);
 			//transform.position = transform.position + Vector3.left*Speed ;
 		}
 		if(Input.GetKey ("a"))
 		{
-			MoveBone.AddForce (Vector3.left * 50*speed);
+			RB.AddForce (Vector3.left * 50*speed);
 			//transform.position = transform.position + Vector3.right*Speed;
 		}
 		if (Input.GetKey ("space")){
-
-			MoveBone.AddForce (Vector3.up * 50*speed);
-			//GetComponent<Rigidbody>().velocity += jumpSpeed * Vector3.up;
+			
 		}
 	}
 	void FixedUpdate ()
 	{
-		GetComponent<Rigidbody>().AddForce (Vector3.up * upSpeed);
 
-<<<<<<< HEAD:New Colour Man/Assets/Scripts/PlayerController.cs
 		if(RB.velocity.magnitude > maxSpeed)
 		{
 			RB.velocity = RB.velocity.normalized * maxSpeed;
 		}
-=======
->>>>>>> origin/master:New Colour Man/Assets/Scripts/RagdollController.cs
 
 	}
 	void OnTriggerEnter(Collider other)
