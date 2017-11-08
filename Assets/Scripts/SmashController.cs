4fb64389ee66d7f24ca124e5629829a6552fd0c4
Quaade94 <larsquaadeconnect@gmail.com>
2017-11-08 11:10:50 +0100

    Smash uden lag

--- Assets/Scripts/SmashController.cs
+++ Assets/Scripts/SmashController.cs
@@ -3,30 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SmashController : MonoBehaviour {
-	float originalY = 0;
-	float maxY = 3.69f;
-	float y;
-	bool ground = true;
-	int count = 0;
 
-	// Use this for initialization
-	void Start () {
-		//this.originalY = this.transform.position.y;
+	float speed = 2;
+	float start;
+
+	void Start(){
+		start = transform.position.y;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		count++;
-		if (this.transform.position.y <= maxY && count<1000) {
-			y = y+0.01f;
-		}
-		if (this.transform.position.y >= originalY+0.1f && count > 1000) {
-			y = y - 0.1f;
-		}
-		if (count > 1500) {
-			count = 0;
-		}
-		transform.position = new Vector3 (transform.position.x, y, transform.position.z); 
+
+		transform.position = new Vector3 (transform.position.x,  start + Mathf.Sin(Time.time * speed)*1.75f, transform.position.z); 
 	}
 
 }
