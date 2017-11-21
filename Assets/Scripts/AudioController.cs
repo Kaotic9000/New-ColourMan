2f070bc6ec9de86a218b1db40c8e295c230eed9a
Unknown <msc95x@gmail.com>
2017-11-21 15:51:55 +0100

    Sound fix for levels

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    public Slider slider;
+
+    private static AudioController instance = null;
+    public static AudioController Instance
+    {
+        get { return instance; }
+    }
+
+    public void Start()
+    {
+        slider.onValueChanged.AddListener(delegate { ValueChangedCheck(); });
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void ValueChangedCheck()
+    {
+        Debug.Log(slider.value);
+    }
+}
